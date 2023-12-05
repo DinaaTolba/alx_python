c42e01fab9a84ce7801bbb0b3bab52015036de48
DinaTolba <magdydina2002@gmail.com>
2023-12-05 20:13:01 +0200

    python new task

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+'''''
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-  print(number,"is postive")
+  print(number,"is positive")
 elif number<0:
   print(number,"is negative")
 else:
   print(number,"is zero")
+  '''
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+
+print(number,"is positive" if number > 0 else "is zero" if number == 0 else "is negative")
