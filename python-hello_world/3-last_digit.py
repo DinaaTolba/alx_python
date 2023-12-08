88a0f21edb452db5badf47d20acb68e5f508b381
DinaTolba <magdydina2002@gmail.com>
2023-12-08 21:25:23 +0200

    taskk

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""""
 import random
 number = random.randint(-10000, 10000)
 sign = '-' if number < 0 else ''
@@ -12,7 +13,30 @@ if number>0:
             print("Last digit of", number,"is",last_digit,"and is 0")
 
 elif number<0:
+    if last_digit <6 and last_digit  !=0 :
+        print("Last digit of", number,"is",sign, last_digit,"and is less than 6 and not 0")
+
+else:
     if last_digit == 0:
         print("Last digit of", number,"is",last_digit,"and is 0")
+        """
+import random
+
+number = random.randint(-10000, 10000)
+sign = '-' if number < 0 else ''
+last_digit = abs(number) % 10
+
+if number > 0:
+    if last_digit > 5:
+        print("Last digit of", number, "is", last_digit, "and is greater than 5")
     else:
-        print("Last digit of", number,"is",sign, last_digit,"and is less than 6 and not 0")
+        if last_digit == 0:
+            print("Last digit of", number, "is", last_digit, "and is 0")
+
+elif number < 0:
+    if last_digit < 6 and last_digit != 0:
+        print("Last digit of", number, "is", sign, last_digit, "and is less than 6 and not 0")
+
+else:
+    if last_digit == 0:
+        print("Last digit of", number, "is", last_digit, "and is 0")
