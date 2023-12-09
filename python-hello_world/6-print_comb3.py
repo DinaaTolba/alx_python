fb3cb97bfbdfe0122ef0d3d0af24f58b609ec596
DinaTolba <magdydina2002@gmail.com>
2023-12-09 17:23:08 +0200

    ohhhh

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,14 +1,6 @@
-'''
-for i in range(1, 10):
-    for j in range(i + 1, 10):
-        if i != 8 or j != 9:
-            print("{:0}{:0}{}".format(i % 10, j % 10, ", " if i < 9 or (i == 8 and j == 9) else "[\"].format("), end="")
-        else:
-            print("{:0}{:0} ".format(i % 10, j % 10))
-'''
 for i in range(1, 10):
     for j in range(i + 1, 10):
         format_str = "{:02d}, " if i < 9 or (i == 8 and j == 9) else "{:02d}[""].format("
         print(format_str.format(i * 10 + j), end="")
     
-    
+ 
