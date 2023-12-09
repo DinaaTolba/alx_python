ef10adbab48d4ffd74f7e2b193dde6ff0f65e2b9
DinaTolba <magdydina2002@gmail.com>
2023-12-09 17:12:05 +0200

    I hope it is the last edit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,14 @@
+"""
 for i in range(10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:  
             print(f"{i}{j}, ", end="")
         else:
             print(f"{i}{j}")
+            """
+for i in range(1, 10):
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9:
+            print("{:0}{:0}{}".format(i % 10, j % 10, ", " if i < 9 or (i == 8 and j == 9) else "[\"].format("), end="")
+        else:
+            print("{:0}{:0} ".format(i % 10, j % 10))
