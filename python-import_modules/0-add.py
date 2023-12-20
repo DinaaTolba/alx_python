e383b1b593e57967d914b37f28907b940f601a51
DinaTolba <magdydina2002@gmail.com>
2023-12-20 22:51:45 +0200

    add function

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Save this code in a file named add_0.py
+
+# Define the add function
+def add(a, b):
+    return a + b
+
+a = 1
+b = 2
+
+# Print the result using string format
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
