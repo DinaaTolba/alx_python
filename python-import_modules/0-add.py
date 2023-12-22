c57712891694d03bcd2ef8a75bb7c558f6baff6b
DinaTolba <magdydina2002@gmail.com>
2023-12-22 18:21:42 +0200

    hoping

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,15 +1,8 @@
-# Save this code in a file named add_0.py
+#!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
 
-# Define the add function
-def add(a, b):
-    return a + b
-
-# Check if the script is being run directly
 if __name__ == "__main__":
-    # Assign values to variables
-    a = 1
-    b = 2
-
-    # Print the result using string format
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
---
+++ python-import_modules/1-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+import sys
+
+# Get the number of arguments
+num_args = len(sys.argv) - 1  # subtract 1 for the script name itself
+
+# Print the number of arguments
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+else:
+    print(f"{num_args} arguments:")
+
+# Print the list of arguments
+for i in range(1, num_args + 1):
+    print(f"{i}: {sys.argv[i]}")
+
+# Add a newline for better formatting
+print()
+'''
+print("hello")
