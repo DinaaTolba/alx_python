fef08ece9d1c8bd475b72b00d2e01c6b0203bce8
DinaTolba <magdydina2002@gmail.com>
2024-01-07 22:30:30 +0200

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,35 @@
+'''size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message size must be >= 0'''
 class Square:
-    size =0
+    """
+    This class represents a square.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance with a given size.
+    """
+
+    # Class attribute, not really needed in this case
+    size = 0
+
     def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If the provided size is not an integer.
+            ValueError: If the provided size is less than 0.
+        """
+        # Initialize the instance attribute 'size'
         self.size = size
+
+        # Validate the size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
-            raise TypeError("size must be >= 0")
-        else:
-            self.size = size
+            raise ValueError('size must be >= 0')
