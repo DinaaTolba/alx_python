63829eb9c163841bf037669d0e113586a9629dee
DinaTolba <magdydina2002@gmail.com>
2024-01-07 22:35:46 +0200

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -25,11 +25,10 @@ class Square:
             TypeError: If the provided size is not an integer.
             ValueError: If the provided size is less than 0.
         """
-        # Initialize the instance attribute 'size'
-        self.size = size
-
         # Validate the size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
